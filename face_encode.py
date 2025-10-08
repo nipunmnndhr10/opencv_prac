554aaf813fa756cf43a46250e83da7d071ae999a
nipunmnndhr10 <nipun16man@gmail.com>
2025-10-08 17:21:06 +0545

    minor changes

--- face_encode.py
+++ face_encode.py
@@ -1,7 +1,11 @@
 import face_recognition
+import os
 
 face_image = face_recognition.load_image_file('faces/me.png')
 
+# [0] here means to extract only one face encoding from the image
 face_encoding = face_recognition.face_encodings(face_image)[0]
 
+# print(face_image)
 print(face_encoding)
+# print(os.path.splitext('emma.jpeg')[0])
